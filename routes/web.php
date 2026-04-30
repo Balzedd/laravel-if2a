766b7f6af8dcef4d93af8fe9d0763006de78cf7a
Balzedd <baldzzzzeddd@gmail.com>
2026-04-30 15:05:19 +0700

    12
    komirt 12

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\FakultasController;
 use App\Http\Controllers\PeriodeController;
+use App\Http\Controllers\ProdiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,3 +15,5 @@ Route::get('/tentang', function () {
 Route::resource('/fakultas', FakultasController::class);
 
 Route::resource('/periodes', PeriodeController::class);
+
+Route::get('/prodi', [ProdiController::class, 'index']);
